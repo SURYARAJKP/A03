42df80589432fd7296c81b7306ace0496ead9197
Surya raj <suryarajkp@gmail.com>
2018-10-29 21:10:17 -0500

    commit

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,29 +1,9 @@
 
 
 
-function roundit(which){
-return Math.round(which*100)/100
-}
-
-function cmconvert(){
-with (document.cminch){
-feet.value = roundit(cm.value/30.84);
-inch.value = roundit(cm.value/2.54);
-}
-}
-
-function inchconvert(){
-with (document.cminch){
-cm.value = roundit(inch.value*2.54);
-feet.value=roundit(inch.value/12);
-}
-}
-
-function feetconvert(){
-with (document.cminch){
-cm.value=roundit(feet.value*30.48);
-inch.value=roundit(feet.value*12);
-}
-}
-
+function percentageCalculator() {
+    percent = document.percentage.percent.value/100;
+    howMuch = percent*document.percentage.howMuch.value;
+    document.percentage.output.value = howMuch;
+    }
 
